c22d54d96c8aded89c5c3953c95397fc7336d06a
ardszsantos <lokgamer01@gmail.com>
2025-03-13 18:46:24 -0300

    started developing

---
+++ src/components/navBar.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+
+
+export const Navbar: React.FC = () => {
+
+
+    return (
+        <header>
+            <p>PEDRO POULOS</p>
+            <nav>
+                <a href="#">Work</a>
+                <a href="#">Journey</a>
+                <a href="#">Testimonials</a>
+                <a href="#">Contact</a>
+            </nav>
+            <p>Beyond the Bio</p>
+        </header>
+    );
+};
---
+++ src/pages/heroPage.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+
+
+export const HeroPage: React.FC = () => {
+
+
+    return (
+        <section className="heroSection">
+            <div className="leftSide">
+                <p>Heyyo, I’m </p>
+                <h1>PEDRO POULOS</h1>
+                <p>A curiosity driven frontend developer with design chops.</p>
+                <p>I believe its OK to be you, You is dope!</p>
+                <div className="socials">
+
+                </div>
+            </div>
+            <div className="rightSide">
+                <img src="" alt="" />
+            </div>
+        </section>
+    );
+};
