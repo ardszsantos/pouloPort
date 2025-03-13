6fb67212e2f08aa656b1533e36ee656a7ab1b335
ardszsantos <lokgamer01@gmail.com>
2025-03-13 18:46:07 -0300

    .

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from './components/navBar'
+import { HeroPage } from './pages/heroPage'
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <HeroPage/>
+    </>
+  )
+}
+
+export default App
