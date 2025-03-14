b5bd94fd395b1019bc57a0a1d115897df4b5c6e3
ardszsantos <lokgamer01@gmail.com>
2025-03-14 13:39:37 -0300

    finished hero page

--- src/pages/heroPage.tsx
+++ src/pages/heroPage.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-
+import music from "../assets/music.png";
+import { FaFigma, FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 
 export const HeroPage: React.FC = () => {
@@ -7,17 +9,21 @@ export const HeroPage: React.FC = () => {
 
     return (
         <section className="heroSection">
-            <div className="leftSide">
-                <p>Heyyo, I’m </p>
-                <h1>PEDRO POULOS</h1>
-                <p>A curiosity driven frontend developer with design chops.</p>
-                <p>I believe its OK to be you, You is dope!</p>
-                <div className="socials">
-
+            <div className="heroWrapper">
+                <div className="leftSide">
+                    <p>Heyyo, I’m </p>
+                    <h1>PEDRO POULOS</h1>
+                    <p>A curiosity driven frontend developer with design chops.</p>
+                    <p>I believe its OK to be you, You is dope!</p>
+                    <div className="socials">
+                        <FaGithub size={24} />
+                        <FaXTwitter size={24} />
+                        <FaFigma size={24} />
+                    </div>
                 </div>
-            </div>
-            <div className="rightSide">
-                <img src="" alt="" />
+                <div className="rightSide">
+                    <img src={music} alt="" />
+                </div>
             </div>
         </section>
     );
