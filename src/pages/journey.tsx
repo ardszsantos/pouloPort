24656c84b0b263516cf2aa5e27ecf83d8a15082c
ardszsantos <lokgamer01@gmail.com>
2025-03-14 13:39:57 -0300

    started deving the journey page

---
+++ src/pages/journey.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+
+
+export const JourneyPage: React.FC = () => {
+
+
+    return (
+        <section className="journeySection">
+            <h1>A BIT 'BOUT MY JOURNEY</h1>
+            <p>
+            I’m a freelance frontend developer based in Ahmedabad, India, with almost 6 years of experience. My journey started at an MNC, then I moved to a startup where I had the chance to build some cool projects that taught me a ton. I began with JavaScript, built apps using Angular, and then switched to React, where I had the opportunity to work on projects from scratch that really shaped my skills in JS and UI development.
+            I don't dive into the back-end too often, but in case you're wondering, I know Node.js, Express & MongoDB. Just a peek behind the curtain!
+            </p>
+            <p>I’ve always been passionate about working on projects that push me to grow, learn new things, and collaborate with great people. This led me to take the leap into freelancing, where I’ve had the chance to work with awesome clients. Right now, I’m working with a cool startup, building sleek UIs with Next.js, Typescript, Tailwind and more.            </p>
+        </section>
+    );
+};
