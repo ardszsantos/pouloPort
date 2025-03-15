e88a1150f6c58c44bded59e8b25d983f118b65c7
ardszsantos <lokgamer01@gmail.com>
2025-03-15 14:36:28 -0300

    hamburguer-react

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-
+import { Sling as Hamburger } from "hamburger-react";
 
 
 export const Navbar: React.FC = () => {
-
+    const [isOpen, setOpen] = useState(false);
 
     return (
         <header>
             <p>PEDRO POULOS</p>
-            <nav>
+            <div className="mobileMenuIcon">
+                <Hamburger toggled={isOpen} toggle={setOpen} size={24} />
+            </div>
+            <nav className={isOpen ? "navOpen" : "navClosed"}>
                 <a href="#">Work</a>
                 <a href="#">Journey</a>
                 <a href="#">Testimonials</a>
