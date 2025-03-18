aa6a3cb5a81cbb624148f70a732b63cbd49a4d10
ardszsantos <lokgamer01@gmail.com>
2025-03-18 16:37:17 -0300

    .

--- src/pages/journey.tsx
+++ src/pages/journey.tsx
@@ -10,7 +10,7 @@ export const JourneyPage: React.FC = () => {
     return (
         <section className="journeySection">
             <h1>A BIT 'BOUT MY JOURNEY</h1>
-            <p>
+            <p className="firstP">
             I’m a freelance frontend developer based in Ahmedabad, India, with almost 6 years of experience. My journey started at an MNC, then I moved to a startup where I had the chance to build some cool projects that taught me a ton. I began with JavaScript, built apps using Angular, and then switched to React, where I had the opportunity to work on projects from scratch that really shaped my skills in JS and UI development.
             I don't dive into the back-end too often, but in case you're wondering, I know Node.js, Express & MongoDB. Just a peek behind the curtain!
             </p>
@@ -31,9 +31,9 @@ export const JourneyPage: React.FC = () => {
             </div>
 
             <p>
-            Fun fact : I’ve been learning Figma just for fun, mainly to create templates and get a feel for how the tool works. It’s been a great way to boost my creativity and help me design UIs more intuitively. One day, I thought, what better way to apply my Figma knowledge than by designing my own portfolio? And here it is – my very first Figma project!
+            <b>Fun fact</b> : I’ve been learning Figma just for fun, mainly to create templates and get a feel for how the tool works. It’s been a great way to boost my creativity and help me design UIs more intuitively. One day, I thought, what better way to apply my Figma knowledge than by designing my own portfolio? And here it is – my very first Figma project!
             </p>
-            <p>Learning advanced TypeScript is something I’m working on, and I try to dedicate time to it whenever I can.
+            <p className="actualLastP">Learning advanced TypeScript is something I’m working on, and I try to dedicate time to it whenever I can.
             </p>
         </section>
     );
