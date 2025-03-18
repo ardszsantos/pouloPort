7e4234c85b2bfcae68125e310c4ea36acb782bfb
ardszsantos <lokgamer01@gmail.com>
2025-03-18 16:58:23 -0300

    footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+export const Footer: React.FC = () => {
+
+    return (
+        <footer>
+            <div className="col">
+                <h1>Pedro Poulos</h1>
+                <p>All Rights Reserved, 1638</p>
+            </div>
+
+
+            
+        </footer>
+    );
+};
